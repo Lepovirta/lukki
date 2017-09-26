673995d508225423688731d5a628a34ab164eef8
Jaakko Pallari <jkpl@lepo.io>
2017-09-26 19:55:03 +0100

    Google Java formatting

--- cli/src/main/java/io/lepo/lukki/cli/Main.java
+++ cli/src/main/java/io/lepo/lukki/cli/Main.java
@@ -3,8 +3,8 @@ package io.lepo.lukki.cli;
 import io.lepo.lukki.core.Crawler;
 
 public class Main {
-    public static void main(String[] args) {
-        Crawler crawler = new Crawler();
-        crawler.run("https://lepo.io/");
-    }
+  public static void main(String[] args) {
+    Crawler crawler = new Crawler();
+    crawler.run("https://lepo.io/");
+  }
 }
--- core/src/main/java/io/lepo/lukki/core/Crawler.java
+++ core/src/main/java/io/lepo/lukki/core/Crawler.java
@@ -21,94 +21,99 @@ import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Phaser;
 
 public class Crawler {
-    private static final Logger log = LoggerFactory.getLogger(Crawler.class);
+  private static final Logger log = LoggerFactory.getLogger(Crawler.class);
 
-    private final CloseableHttpAsyncClient httpClient;
-    private final ForkJoinPool pool;
-    private final Phaser phaser;
-    private final ConcurrentMap<String, Boolean> visitedUrls;
+  private final CloseableHttpAsyncClient httpClient;
+  private final ForkJoinPool pool;
+  private final Phaser phaser;
+  private final ConcurrentMap<String, Boolean> visitedUrls;
 
-    public Crawler() {
-        httpClient = HttpAsyncClients.createDefault();
-        pool = new ForkJoinPool(2);
-        phaser = new Phaser(1);
-        visitedUrls = new ConcurrentHashMap<>();
+  public Crawler() {
+    httpClient = HttpAsyncClients.createDefault();
+    pool = new ForkJoinPool(2);
+    phaser = new Phaser(1);
+    visitedUrls = new ConcurrentHashMap<>();
+  }
+
+  public void run(String url) {
+    log.debug("Starting");
+    httpClient.start();
+
+    enqueueUrl(url, url);
+    phaser.arriveAndAwaitAdvance();
+    log.debug("Finished");
+
+    try {
+      httpClient.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    public void run(String url) {
-        log.debug("Starting");
-        httpClient.start();
-
-        enqueueUrl(url, url);
-        phaser.arriveAndAwaitAdvance();
-        log.debug("Finished");
-
-        try {
-            httpClient.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void enqueueUrl(final String rootUrl, final String url) {
-        visitedUrls.computeIfAbsent(url, k -> {
-            phaser.register();
-            pool.execute(() -> {
+  private void enqueueUrl(final String rootUrl, final String url) {
+    visitedUrls.computeIfAbsent(
+        url,
+        k -> {
+          phaser.register();
+          pool.execute(
+              () -> {
                 getUrl(rootUrl, url);
-            });
-            return true;
+              });
+          return true;
         });
-    }
+  }
 
-    private void getUrl(String rootUrl, String url) {
-        try {
-            HttpGet httpGet = new HttpGet(url);
-            httpClient.execute(httpGet, new FutureCallback<HttpResponse>() {
-                @Override
-                public void completed(HttpResponse result) {
-                    HttpEntity entity = result.getEntity();
-                    try (InputStream contentStream = entity.getContent()) {
-                        Document document = Jsoup.parse(contentStream, null, url);
-                        Elements anchorElements = document.select("a");
-                        for (Element anchor : anchorElements) {
-                            String href = anchor.absUrl("href");
-                            if (href != null && href.startsWith(rootUrl)) {
-                                enqueueUrl(rootUrl, href);
-                            }
-                        }
-                        resultToOutput(url, "OK!");
-                    } catch (IOException ex) {
-                        resultToOutput(url, ex.getMessage());
-                    } finally {
-                        phaser.arriveAndDeregister();
-                    }
+  private void getUrl(String rootUrl, String url) {
+    try {
+      HttpGet httpGet = new HttpGet(url);
+      httpClient.execute(
+          httpGet,
+          new FutureCallback<HttpResponse>() {
+            @Override
+            public void completed(HttpResponse result) {
+              HttpEntity entity = result.getEntity();
+              try (InputStream contentStream = entity.getContent()) {
+                Document document = Jsoup.parse(contentStream, null, url);
+                Elements anchorElements = document.select("a");
+                for (Element anchor : anchorElements) {
+                  String href = anchor.absUrl("href");
+                  if (href != null && href.startsWith(rootUrl)) {
+                    enqueueUrl(rootUrl, href);
+                  }
                 }
+                resultToOutput(url, "OK!");
+              } catch (IOException ex) {
+                resultToOutput(url, ex.getMessage());
+              } finally {
+                phaser.arriveAndDeregister();
+              }
+            }
 
-                @Override
-                public void failed(Exception ex) {
-                    try {
-                        resultToOutput(url, ex.getMessage());
-                    } finally {
-                        phaser.arriveAndDeregister();
-                    }
-                }
+            @Override
+            public void failed(Exception ex) {
+              try {
+                resultToOutput(url, ex.getMessage());
+              } finally {
+                phaser.arriveAndDeregister();
+              }
+            }
 
-                @Override
-                public void cancelled() {
-                    try {
-                        resultToOutput(url, "cancelled!");
-                    } finally {
-                        phaser.arriveAndDeregister();
-                    }
-                }
-            });
-        } catch (IllegalArgumentException ex) {
-            resultToOutput(url, ex.getMessage());
-            phaser.arriveAndDeregister();
-        }
+            @Override
+            public void cancelled() {
+              try {
+                resultToOutput(url, "cancelled!");
+              } finally {
+                phaser.arriveAndDeregister();
+              }
+            }
+          });
+    } catch (IllegalArgumentException ex) {
+      resultToOutput(url, ex.getMessage());
+      phaser.arriveAndDeregister();
     }
+  }
 
-    private void resultToOutput(String url, String message) {
-        System.out.println(url + ": " + message);
-    }
+  private void resultToOutput(String url, String message) {
+    System.out.println(url + ": " + message);
+  }
 }
