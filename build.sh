60c14bb1d3c3cedc06440c42174c1201868a7719
Jaakko Pallari <jkpl@lepovirta.org>
2019-04-07 14:29:06 +0300

    Download dependencies before build

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ cd "$(dirname "$0")"
 
 BUILDINFO_PKG="github.com/Lepovirta/lukki/internal/buildinfo"
 VERSION=$(git describe --match 'v[0-9]*' | sed 's/^v//')
+
+go mod download
 go build -v -ldflags "-X $BUILDINFO_PKG.Version=$VERSION" -o lukki
