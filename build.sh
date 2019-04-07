3265a679ab9c4608bc1566b2b621eebad0cec262
Jaakko Pallari <jkpl@lepovirta.org>
2019-04-07 15:03:47 +0300

    Cross-compilation

--- build.sh
+++ build.sh
@@ -4,6 +4,38 @@ cd "$(dirname "$0")"
 
 BUILDINFO_PKG="github.com/Lepovirta/lukki/internal/buildinfo"
 VERSION=$(git describe --match 'v[0-9]*' | sed 's/^v//')
+PLATFORMS=(
+    "linux/arm"
+    "linux/arm64"
+    "linux/386"
+    "linux/amd64"
+    "darwin/amd64"
+    "windows/386"
+    "windows/amd64"
+)
 
-go mod download
-go build -v -ldflags "-X $BUILDINFO_PKG.Version=$VERSION" -o lukki
+setup() {
+    mkdir -p out
+    go mod download
+}
+
+build_go() {
+    local suffix
+    if [ "$GOOS" = "windows" ]; then
+        suffix=".exe"
+    fi
+    go build -v -ldflags "-X $BUILDINFO_PKG.Version=$VERSION" -o "out/lukki-${GOOS}-${GOARCH}${suffix:-}"
+}
+
+build_all() {
+    local platform_split
+    for platform in "${PLATFORMS[@]}"; do
+        platform_split=(${platform//\// })
+        GOOS=${platform_split[0]}
+        GOARCH=${platform_split[1]}
+        build_go
+    done
+}
+
+setup
+build_all
