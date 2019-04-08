e15dab5d620974eb3298edf1f40806074fa64e8b
Jaakko Pallari <jkpl@lepovirta.org>
2019-04-08 22:35:38 +0300

    Fix: strip port from home hosts

---
+++ config/config_test.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestConfigInit(t *testing.T) {
+	a := assert.New(t)
+
+	conf := Config{URLs: []string{"http://localhost:1313/", "https://lepo.group/"}}
+	err := conf.Init()
+
+	a.NoError(err)
+	a.Equal(defaultElements, conf.Elements)
+	a.ElementsMatch([]string{"localhost", "lepo.group"}, conf.HomeHosts)
+	a.Equal(userAgent, conf.UserAgent)
+}
